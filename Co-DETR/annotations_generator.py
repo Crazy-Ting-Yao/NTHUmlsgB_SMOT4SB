e9300402e6868e2d18ce91f73eb1cb084eb10af5
YanFu Chen <aaaronyanfu@gmail.com>
2025-05-06 22:00:25 +0800

    Add python file

---
+++ Co-DETR/annotations_generator.py
@@ -0,0 +1,46 @@
+import json
+from pathlib import Path
+from PIL import Image
+
+file_extensions = ["*.jpg", "*.jpeg", "*.png", "*.tif", "*.gif"]
+root_dir = Path("Co-DETR/dataset/SMOT4SB/private_test")
+output_dir = Path("Co-DETR/dataset/SMOT4SB/private_test_anns")
+output_dir.mkdir(parents=True, exist_ok=True)
+
+for folder in sorted(root_dir.iterdir()):
+    if not folder.is_dir():
+        continue
+
+    folder_name = folder.name
+    images_info = []
+
+    for extension in file_extensions:
+        for filename in sorted(folder.glob(extension)):
+            try:
+                image = Image.open(filename)
+                width, height = image.size
+                images_info.append([filename.name, int(height), int(width)])
+            except Exception as e:
+                print(f"Error reading {filename}: {e}")
+
+    images = [
+        {
+            "file_name": image_info[0],
+            "height": image_info[1],
+            "width": image_info[2],
+            "id": i,
+        }
+        for i, image_info in enumerate(images_info, start=1)
+    ]
+
+    ann_file = {
+        "categories": [{"id": 1, "name": "bird"}],
+        "annotations": [],
+        "images": images,
+    }
+
+    json_path = output_dir / f"{folder_name}.json"
+    with open(json_path, 'w') as fp:
+        json.dump(ann_file, fp)
+
+    print(f"Saved: {json_path}")
---
+++ Co-DETR/run_test.py
@@ -0,0 +1,36 @@
+import subprocess
+from pathlib import Path
+
+config_path = 'Co-DETR/projects/configs/co_deformable_detr/co_deformable_detr_swin_large_900q_3x_smot4sb.py'
+checkpoint_path = 'Co-DETR/model/epoch_12.pth'
+output_dir = Path('results_12')
+output_dir.mkdir(parents=True, exist_ok=True)
+
+root_img_dir = Path('Co-DETR/dataset/SMOT4SB/private_test')
+ann_dir = Path('Co-DETR/dataset/SMOT4SB/private_test_anns')
+
+for folder in sorted(root_img_dir.iterdir()):
+    if not folder.is_dir():
+        continue
+
+    folder_name = folder.name
+    ann_file = ann_dir / f'{folder_name}.json'
+    img_prefix = folder
+
+    json_prefix = output_dir / f'results_{folder_name}'
+
+    command = [
+        'python', 'tools/test.py', config_path,
+        checkpoint_path,
+        '--format-only',
+        '--cfg-options',
+        f'data.test.ann_file={ann_file}',
+        f'data.test.img_prefix={img_prefix}',
+        '--eval-options',
+        f'jsonfile_prefix={json_prefix}',
+    ]
+
+    print(f'Running inference for folder {folder_name}...')
+    subprocess.run(command)
+
+print("All inference tasks completed.")
